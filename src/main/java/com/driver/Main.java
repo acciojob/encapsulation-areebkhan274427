b69d85eeed22f38637077e205ad016aba6f805dd
Areeb <areebkhan274427@gmail.com>
2022-12-10 17:10:33 +0530

    Changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+
+        //obj.name="Areeb";  //error will throw
+
+        obj.setName("Areeb");
+        obj.getName();
+    }
 }
